5e4bd54c09bccceecf2a6955459c71a4b7b3c242
Azam Amonov <azamamonov555@gmail.com>
2023-11-07 19:19:55 +0500

    fileProfile finished

--- FileExplorer.Demo.Infrastructure/Common/MapperProfile/FileProfile.cs
+++ FileExplorer.Demo.Infrastructure/Common/MapperProfile/FileProfile.cs
@@ -1,6 +1,17 @@
+using AutoMapper;
+using FIleExplorer.Demo.Application.FileStorage.Models.Storage;
+
 namespace FileExplorer.Demo.Infrastructure.Common.MapperProfile;
 
-public class FileProfile
+public class FileProfile: Profile
 {
-    
+   public FileProfile()
+   {
+       CreateMap<FileInfo, StorageFile>()
+                       .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                       .ForMember(dest => dest.Path, opt => opt.MapFrom(src => src.FullName))
+                       .ForMember(dest => dest.DirectoryPath, opt => opt.MapFrom(src => src.DirectoryName))
+                       .ForMember(dest => dest.Size, opt => opt.MapFrom(src => src.Length))
+                       .ForMember(dest => dest.Extension, opt => opt.MapFrom(src => src.Extension));
+   }
 }
