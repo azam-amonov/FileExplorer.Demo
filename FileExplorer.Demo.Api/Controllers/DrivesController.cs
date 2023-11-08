f260818b02c6d1c5e70a648469d26c428027a6c0
Azam Amonov <azamamonov555@gmail.com>
2023-11-08 18:04:41 +0500

    drive controller ass

--- FileExplorer.Demo.Api/Controllers/DrivesController.cs
+++ FileExplorer.Demo.Api/Controllers/DrivesController.cs
@@ -1,6 +1,26 @@
-namespace FileExplorer.Demo.Api.Controllers;
+using AutoMapper;
+using FileExplorer.Demo.Api.Models.DTOs;
+using FIleExplorer.Demo.Application.FileStorage.Models.Storage;
+using FIleExplorer.Demo.Application.FileStorage.Services;
+using Microsoft.AspNetCore.Mvc;
 
-public class DrivesController
+namespace FileExplorer.Demo.Api.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class DrivesController : ControllerBase
 {
+    private readonly IMapper _mapper;
     
+    public DrivesController(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAsync([FromServices] IDriveService driveService)
+    {
+        var data = await driveService.GetAsync();
+        var result = _mapper.Map<IEnumerable<StorageDriveDto>>(data);
+        return result.Any() ? Ok(result) : NoContent();
+    }
 }
