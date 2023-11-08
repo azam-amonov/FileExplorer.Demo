e60a50830449b3675a20cb0aa46b4dee826bd9ea
Azam Amonov <azamamonov555@gmail.com>
2023-11-08 18:40:09 +0500

    add file controller

--- FileExplorer.Demo.Api/Controllers/FilesController.cs
+++ FileExplorer.Demo.Api/Controllers/FilesController.cs
@@ -1,6 +1,32 @@
-namespace FileExplorer.Demo.Api.Controllers;
+using FIleExplorer.Demo.Application.FileStorage.Models.Filtering;
+using FIleExplorer.Demo.Application.FileStorage.Services;
+using Microsoft.AspNetCore.Mvc;
 
-public class FilesController
+namespace FileExplorer.Demo.Api.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class FilesController : ControllerBase
 {
+    private readonly IWebHostEnvironment _hostEnvironment;
+    private readonly IFileProcessingService _fileProcessingService;
+
+    public FilesController(IWebHostEnvironment hostEnvironment, IFileProcessingService processingService)
+    {
+        _hostEnvironment = hostEnvironment;
+        _fileProcessingService = processingService;
+    }
+    [HttpGet("root/files/filter")]
+    public async ValueTask<IActionResult> GetFileSummary()
+    {
+        var result = await _fileProcessingService.GetFilterDataModelAsync(_hostEnvironment.WebRootPath);
+        return Ok(result);
+    }
+
+    [HttpGet("root/files/by-filter")]
+    public async ValueTask<IActionResult> GetFileByFilter([FromQuery] StorageFileFilterModel filterModel)
+    {
+        var files = await _fileProcessingService.GetByFilterAsync(filterModel);
+        return files.Any() ? Ok(files) : NotFound(files);
+    }
     
 }
