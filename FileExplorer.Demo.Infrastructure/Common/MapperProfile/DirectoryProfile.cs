d4ccd92979d2020517a6408b97867dca2edba95a
Azam Amonov <azamamonov555@gmail.com>
2023-11-08 14:41:21 +0500

    Directory profile mapping finished

--- FileExplorer.Demo.Infrastructure/Common/MapperProfile/DirectoryProfile.cs
+++ FileExplorer.Demo.Infrastructure/Common/MapperProfile/DirectoryProfile.cs
@@ -1,8 +1,15 @@
 using AutoMapper;
+using FIleExplorer.Demo.Application.FileStorage.Models.Storage;
 
 namespace FileExplorer.Demo.Infrastructure.Common.MapperProfile;
 
 public class DirectoryProfile : Profile
 {
-    
+    public DirectoryProfile()
+    {
+        CreateMap<DirectoryInfo, StorageDirectory>()
+            .ForMember(src => src.Name, opt => opt.MapFrom(dest => dest.Name))
+            .ForMember(src => src.Path, opt => opt.MapFrom(dest => dest.FullName))
+            .ForMember(src => src.ItemsCount, opt => opt.MapFrom(dest => dest.GetFileSystemInfos().Count()));
+    }
 }
